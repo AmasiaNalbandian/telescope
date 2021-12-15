5a36ec2eee4228522f25260c18d7a0d59a2c262b
AmasiaNalbandian <amasia.nalbandian@mitel.com>
2021-12-14 21:49:31 -0500

    added advanced route with params

--- src/api/search/src/routes/query.js
+++ src/api/search/src/routes/query.js
@@ -1,6 +1,6 @@
 const { Router, createError } = require('@senecacdot/satellite');
 const { validateQuery } = require('../bin/validation');
-const search = require('../search');
+const { search, advancedSearch } = require('../search');
 
 const router = Router();
 
@@ -9,6 +9,19 @@ router.get('/', validateQuery, async (req, res, next) => {
     const { text, filter, page, perPage } = req.query;
     res.send(await search(text, filter, page, perPage));
   } catch (error) {
+    next(createError(503, error));
+  }
+});
+
+// route for advanced
+router.get('/advanced', validateQuery, async (req, res, next) => {
+  try {
+    const { postText, authorText, titleText, dateStart, dateEnd, page, perPage } = req.query;
+    res.send(
+      await advancedSearch(postText, authorText, titleText, dateStart, dateEnd, page, perPage)
+    );
+  } catch (error) {
+    console.log('error', error);
     next(createError(503, error));
   }
 });
